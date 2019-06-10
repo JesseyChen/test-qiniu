d3709498da7a358c11b04cf85ed11a8a765764de
summer <chenjunxing1012@gmail.com>
2019-06-10 16:25:58 +0800

    add config and facade

--- src/TestServiceProvider.php
+++ src/TestServiceProvider.php
@@ -1,13 +1,18 @@
 <?php
+
 namespace JesseyChen\Test;
 
 use Illuminate\Support\ServiceProvider;
+use PhpParser\Node\Scalar\MagicConst\Dir;
 
 class TestServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         //
+        $this->publishes([
+            __DIR__ . '/config/qiniu.php' => config_path('qiniu.config')
+        ]);
     }
 
     public function register()
